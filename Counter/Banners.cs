420d4b8f3c741b17c86a90879b5bff9acf9114e7
Edwin Zhang <edwin.x.zhang@gmail.com>
2025-04-17 21:48:07 -0700

    Copyright

--- Counter/Banners.cs
+++ Counter/Banners.cs
@@ -1,3 +1,29 @@
+/**
+    Defines the "Banner" class. The constructor takes three arguments: the probability
+    distribution table of the type of piece the user would like to count (for example
+    5*, 4* within 5*, or 4* standalone), the number of pieces to count, and a label
+    string for the output file. Public methods are provided to generate a probability
+    table for the number of pieces and export a probability table to a csv.
+
+    Copyright (c) 2025 Edwin Xingyun Zhang
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+    software and associated documentation files (the "Software"), to deal in the Software
+    without restriction, including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+    persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies
+    or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+    PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+    FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+    OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+**/
+
 namespace Counter;
 public class Banner
 {
