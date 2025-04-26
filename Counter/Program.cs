25ac1ea99ae6b5d649cd4ae74b677da452ff3697
Edwin Zhang <edwin.x.zhang@gmail.com>
2025-04-25 20:48:29 -0700

    Add support for 4* outfits

--- Counter/Program.cs
+++ Counter/Program.cs
@@ -24,6 +24,13 @@
 
 namespace Counter
 {
+    enum BannerType
+    {
+        FiveStar,
+        FourStarWithinFive,
+        FourStarStandlone
+    }
+
     class Counter
     {
         // TODO:
@@ -57,13 +64,85 @@ namespace Counter
             return successChance;
         }
 
+        static double[] FourStarWithinFiveProbabilityTable()
+        {
+            int maxPulls = 10;
+            double baseProbability = 0.0329;
+            double hardPity10 = 1;
+
+            double[] successChance = new double[maxPulls];
+            double[] failureChance = new double[maxPulls];
+
+            successChance[0] = baseProbability;
+            failureChance[0] = 1 - baseProbability;
+            for (int i = 1; i < 9; i++)
+            {
+                failureChance[i] = Math.Pow(1 - baseProbability, i + 1);
+                successChance[i] = baseProbability * failureChance[i-1];
+            }
+            failureChance[9] = 0;
+            successChance[9] = failureChance[8] * hardPity10;
+
+            return successChance;
+        }
+
+        static double[] FourStarStandaloneProbabilityTable()
+        {
+            int maxPulls = 5;
+            double baseProbability = 0.1;
+            double softPity4 = 0.2;
+            double hardPity5 = 1;
+
+            double[] successChance = new double[maxPulls];
+            double[] failureChance = new double[maxPulls];
+
+            successChance[0] = baseProbability;
+            failureChance[0] = 1 - baseProbability;
+            for (int i = 1; i < 3; i++)
+            {
+                failureChance[i] = Math.Pow(1 - baseProbability, i + 1);
+                successChance[i] = baseProbability * failureChance[i-1];
+            }
+            failureChance[3] = failureChance[2] * (1 - softPity4);
+            failureChance[4] = 0;
+            successChance[3] = failureChance[2] * softPity4;
+            successChance[4] = failureChance[3] * hardPity5;
+
+            return successChance;
+        }
+
         static void Main(string[] args)
         {
             // TODO: Populate these based on user arguments
-            // Hard code to 10-piece 5* evo for demo purposes
-            int totalPieces = 20; // 20 pieces for 10-piece 5* full evolution
-            double[] pullChance = FiveStarPieceProbabilityTable(); // Probability distribution for 5* pieces
-            string label = "FiveStarOutfit" + totalPieces + "Pieces"; // File label
+            // Hard code for demo purposes
+            int totalPieces;
+            BannerType Banner = BannerType.FiveStar;
+
+            double[] pullChance;
+            string label;
+
+            switch(Banner)
+            {
+                case BannerType.FourStarStandlone:
+                    // Four star standalone banner
+                    totalPieces = 9;
+                    pullChance = FourStarStandaloneProbabilityTable();
+                    label = "FourStarStandaloneOutfit" + totalPieces + "Pieces";
+                    break;
+                case BannerType.FourStarWithinFive:
+                    // Four star outfit within five star banner
+                    totalPieces = 9;
+                    pullChance = FourStarWithinFiveProbabilityTable();
+                    label = "FourStarCompanionOutfit" + totalPieces + "Pieces";
+                    break;
+                case BannerType.FiveStar:
+                default:
+                    // Five star banner
+                    totalPieces = 10;
+                    pullChance = FiveStarPieceProbabilityTable();
+                    label = "FiveStarOutfit" + totalPieces + "Pieces";
+                    break;
+            }
 
             // Use the Banner class to generate a probability table
             Banner bannerOutfit = new Banner(pullChance, totalPieces, label);
